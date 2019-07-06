c71baa6696c73b8c41f3668980edc14865043e10
hadinajfi77 <hadinajafi.77@gmail.com>
2019-07-06 13:27:42 +0430

    add shop.js to handle admins routes

---
+++ src/routes/admin.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const router = express.Router();
+
+router.use(bodyParser.urlencoded());
+
+router.get('/add-product', (req, res, next) =>{
+	res.send('<form action="/product" method="POST"><input name="title" type="text"><button type="submitted">Add product</button></form>');
+});
+
+router.post('/product', (req, res, next) => {
+	console.log(req.body);
+	res.redirect('/');
+});
+
+module.exports = router;
