db952a1a1c7ede1dcd387a1065512bccb1f15ad5
hadinajfi77 <hadinajafi.77@gmail.com>
2019-07-06 16:24:47 +0430

    add html pages loading to the code.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
+
+app.use(bodyParser.urlencoded());
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,12 +1,12 @@
+const path = require('path');
+
 const express = require('express');
-const bodyParser = require('body-parser');
+
 
 const router = express.Router();
 
-router.use(bodyParser.urlencoded());
-
 router.get('/add-product', (req, res, next) =>{
-	res.send('<form action="/admin/add-product" method="POST"><input name="title" type="text"><button type="submitted">Add product</button></form>');
+	res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res, next) => {
