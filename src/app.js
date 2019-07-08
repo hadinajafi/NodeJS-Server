af3508a81192901fb876ec3050da73e2b0176073
hadinajfi77 <hadinajafi.77@gmail.com>
2019-07-08 14:39:16 +0430

    add MVC controllers

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const errorController = require('./controller/error');
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -14,12 +15,10 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) =>{	//handling when invalid page request comes
-	res.status(404).render('404', {pageTitle: 'Page Not Found', path: ''});
-});
+app.use(errorController.get404);
 
 app.listen(3000);
 console.log("Server running at port 3000");
