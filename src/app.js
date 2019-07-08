b563df28799d55117fa8660a62c2557db0e381be
hadinajfi77 <hadinajafi.77@gmail.com>
2019-07-08 13:29:21 +0430

    add ejs engine for templating

--- src/app.js
+++ src/app.js
@@ -3,19 +3,22 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const app = express();
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));	//where to find views: ./views folder
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-const app = express();
 
 app.use(bodyParser.urlencoded());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminRoutes.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) =>{	//handling when invalid page request comes
-	res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+	res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 
 app.listen(3000);
