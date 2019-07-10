fb3db3161c0d211632917f29f1a25ed5d49002e9
hadinajfi77 <hadinajafi.77@gmail.com>
2019-07-10 18:53:56 +0430

    added more items to the products attributes

--- src/models/product.js
+++ src/models/product.js
@@ -6,23 +6,26 @@ const p = path.join(rootDir, 'data', 'products.json');
 
 const getProductsFromFile = cb => {		//cb is callback
 	fs.readFile(p, (err, fileContent) => {
-		if(err){
+		if (err) {
 			cb([]);
-		}else{
+		} else {
 			cb(JSON.parse(fileContent));
 		}
 	});
 };
 
 module.exports = class Product {
-	constructor(t) {
-		this.title = t;
+	constructor(name, imgUrl, price, desc) {
+		this.title = name;
+		this.image = imgUrl;
+		this.price = price;
+		this.description = desc;
 	}
 
 	save() {
 		getProductsFromFile(products => {
 			products.push(this);
-			fs.writeFile(p, JSON.stringify(products), err =>{
+			fs.writeFile(p, JSON.stringify(products), err => {
 				console.log(err);
 			});
 		});
